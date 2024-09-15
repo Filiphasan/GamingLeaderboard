186ed948db89924799e982797593a584c42bc487
Hasan Erdal <hasaerda@hotmail.com>
2024-09-15 21:48:52 +0300

    Initial Project, install requirements libraries

---
+++ Api/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿namespace Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        return services;
+    }
+}
